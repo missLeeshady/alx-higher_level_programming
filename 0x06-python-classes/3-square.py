8ca872c6b85354c1d369b7e4c610349b31b9469c
missLeeshady <beadsbyalfiyya@gmail.com>
2022-12-20 10:22:58 -0800

    area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class representation"""
+
+
+class Square:
+    """represents a square"""
+    def __init__(self, size=0):
+        """initialize data"""
+        self.__size = size
+        if size is not int(size):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+
+    def area(self):
+        """return area of square"""
+        result = self.__size * self.__size
+        return result
